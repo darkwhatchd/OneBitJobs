0dd77ae989668e95499b85d8b1daede9e1250737
Daniel Evani <danielevani97@gmail.com>
2021-09-06 13:53:17 -0300

    Vacancy validates title, location, requirements, salary, available, description

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -2,4 +2,12 @@ class Vacancy < ApplicationRecord
   belongs_to :company
 
   has_many :applicants
+
+  validates :title, 
+            :location, 
+            :requirements, 
+            :salary, 
+            :available,
+            presence: true
+  validates :description, length: {minimum:3, maximum:1000}, presence: true
 end
