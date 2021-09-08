768985f3dbdbb1721f99406a66e7b936b03d1926
Daniel Evani <danielevani97@gmail.com>
2021-09-08 15:10:07 -0300

    Sign up page customize, define route after sign in/up

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Onebitjobs
   class Application < Rails::Application
+    config.to_prepare do
+      Devise::RegistrationsController.layout "application"
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
